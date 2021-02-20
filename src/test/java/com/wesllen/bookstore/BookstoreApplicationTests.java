b7679f9256bb7bf8b95aba3b5743959062f5b46b
wesllenalves <wesllenalves@gmail.com>
2021-02-20 15:58:53 -0300

    first commit

---
+++ src/test/java/com/wesllen/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesllen.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
